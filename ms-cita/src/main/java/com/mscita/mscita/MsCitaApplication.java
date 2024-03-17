314847e1dd674051e7d4e3690c6eae72ed01b7bf
gabyportu <gabriela.portugal@ucb.edu.bo>
2024-03-16 22:50:47 -0400

    creacion microservicios

---
+++ ms-cita/src/main/java/com/mscita/mscita/MsCitaApplication.java
@@ -0,0 +1,13 @@
+package com.mscita.mscita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCitaApplication.class, args);
+	}
+
+}
