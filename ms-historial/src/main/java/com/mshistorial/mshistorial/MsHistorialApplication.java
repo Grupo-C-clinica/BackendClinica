3cbed840f3c422998970731c1d11381da2c65670
gabyportu <gabriela.portugal@ucb.edu.bo>
2024-03-16 22:59:43 -0400

    creacion microservicio historial

---
+++ ms-historial/src/main/java/com/mshistorial/mshistorial/MsHistorialApplication.java
@@ -0,0 +1,13 @@
+package com.mshistorial.mshistorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsHistorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsHistorialApplication.class, args);
+	}
+
+}
---
+++ ms-historial/src/test/java/com/mshistorial/mshistorial/MsHistorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mshistorial.mshistorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsHistorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
