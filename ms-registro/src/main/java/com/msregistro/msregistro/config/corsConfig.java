55c59deaf83575474a1884ba8064c8a1943428d5
gabyportu <gabriela.portugal@ucb.edu.bo>
2024-04-26 17:53:06 -0400

    arreglos

--- ms-registro/src/main/java/com/msregistro/msregistro/config/corsConfig.java
+++ ms-registro/src/main/java/com/msregistro/msregistro/config/corsConfig.java
@@ -1,5 +1,4 @@
-package msartista.msartista.config;
-
+package com.msregistro.msregistro.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
