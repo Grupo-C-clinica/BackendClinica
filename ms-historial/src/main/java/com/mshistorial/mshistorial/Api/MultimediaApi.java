1825c648256da0c25fd5e322af1a448a2c756045
lim2002 <limbert.quispe@ucb.edu.bo>
2024-05-16 12:11:33 -0400

    Configuracion Minio

--- ms-historial/src/main/java/com/mshistorial/mshistorial/Api/MultimediaApi.java
+++ ms-historial/src/main/java/com/mshistorial/mshistorial/Api/MultimediaApi.java
@@ -6,6 +6,7 @@ import com.mshistorial.mshistorial.Dto.ResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -18,8 +19,8 @@ public class MultimediaApi {
 
     //Crear una lista demultimedia de un historial
     @PostMapping("/create/{historialId}")
-    public ResponseEntity<ResponseDto<String>> createMultimedia(@PathVariable Integer historialId, @RequestBody List<String> multimediaDtos){
-        multimediaBl.createMultimedia(historialId, multimediaDtos);
+    public ResponseEntity<ResponseDto<String>> createMultimedia(@PathVariable Integer historialId, @RequestParam("files") List<MultipartFile> files){
+        multimediaBl.createMultimedia(historialId, files);
         try {
             return ResponseEntity.ok(new ResponseDto<>(200, "Multimedia creado correctamente", "Success"));
         } catch (Exception e){
@@ -29,13 +30,12 @@ public class MultimediaApi {
 
     // Mostrar todas las imagenes de un historial
     @GetMapping("/all/{historialId}")
-    public ResponseEntity<ResponseDto<List<MultimediaDto>>> findAllByHistorialId(@PathVariable Integer historialId){
-        List<MultimediaDto> multimedia = multimediaBl.findAllByHistorialId(historialId);
+    public ResponseEntity<ResponseDto<List<MultipartFile>>> findAllByHistorialId(@PathVariable Integer historialId){
+            List<MultipartFile> multimedia = multimediaBl.findAllByHistorialId(historialId);
         try {
             return ResponseEntity.ok(new ResponseDto<>(200, multimedia, "Success"));
         } catch (Exception e){
             return ResponseEntity.ok(new ResponseDto<>(500, null, "Error"));
         }
     }
-
 }
--- ms-historial/src/main/java/com/mshistorial/mshistorial/Bl/MultimediaBl.java
+++ ms-historial/src/main/java/com/mshistorial/mshistorial/Bl/MultimediaBl.java
@@ -4,9 +4,17 @@ import com.mshistorial.mshistorial.Dao.MultimediaRepository;
 import com.mshistorial.mshistorial.Dto.MultimediaDto;
 import com.mshistorial.mshistorial.Entity.Historial;
 import com.mshistorial.mshistorial.Entity.Multimedia;
+import io.minio.MinioClient;
+import io.minio.StatObjectArgs;
+import io.minio.errors.MinioException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,30 +24,72 @@ public class MultimediaBl {
     @Autowired
     private MultimediaRepository multimediaRepository;
 
+    @Autowired
+    private MinioClient minioClient;
+
+    @Autowired
+    private MinioBl minioService;
+
+    String bucketName = "software";
+
     //Crear multimeda de un hostorial
-    public void createMultimedia(Integer historialId, List<String> multimedia){
-        for (String multimediaDto : multimedia){
-            Multimedia multimediaEntity = new Multimedia();
-            Historial historial = new Historial();
-            historial.setIdHistorial(historialId);
-            multimediaEntity.setHistorial(historial);
-            multimediaEntity.setMultimedia(multimediaDto);
-            multimediaEntity.setStatus(true);
-            multimediaRepository.save(multimediaEntity);
+    public void createMultimedia(Integer historialId, List<MultipartFile> multimedia){
+        for (MultipartFile multimediaDto : multimedia){
+            try {
+                Multimedia multimediaEntity = new Multimedia();
+                Historial historial = new Historial();
+                historial.setIdHistorial(historialId);
+                multimediaEntity.setHistorial(historial);
+                String multimediaName = multimediaDto.getOriginalFilename();
+                // Sube el archivo a Minio
+                minioClient.putObject(
+                        io.minio.PutObjectArgs.builder()
+                                .bucket(bucketName)
+                                .object(multimediaName) // Cambio aquí: usa solo el nombre del archivo original
+                                .stream(multimediaDto.getInputStream(), multimediaDto.getSize(), -1)
+                                .build()
+                );
+                multimediaEntity.setMultimedia(multimediaName);
+                multimediaEntity.setStatus(true);
+                multimediaRepository.save(multimediaEntity);
+            } catch (MinioException | IOException e) {
+                e.printStackTrace();
+            } catch (NoSuchAlgorithmException e) {
+                throw new RuntimeException(e);
+            } catch (InvalidKeyException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
     //Mostrar multimedia de un historial
-    public List<MultimediaDto> findAllByHistorialId(Integer historialId){
-        List<Multimedia> multimedia = multimediaRepository.findAllByHistorialId(historialId);
-        List<MultimediaDto> multimediaDto = new ArrayList<>();
-        // convertir todos los multimedia en multimedia dto en un for
-        for (Multimedia multimedia1 : multimedia){
-            MultimediaDto multimediaDtos = new MultimediaDto();
-            multimediaDtos.setIdMultimedia(multimedia1.getIdMultimedia());
-            multimediaDtos.setMultimedia(multimedia1.getMultimedia());
-            multimediaDto.add(multimediaDtos);
+    public List<MultipartFile> findAllByHistorialId(Integer historialId) {
+        List<Multimedia> multimediaList = multimediaRepository.findAllByHistorialId(historialId);
+        List<MultipartFile> multipartFiles = new ArrayList<>();
+        for (Multimedia multimediaEntity : multimediaList) {
+            try {
+                // Verificar si el archivo existe en MinIO
+                StatObjectArgs statObjectArgs = StatObjectArgs.builder()
+                        .bucket(bucketName)
+                        .object(multimediaEntity.getMultimedia())
+                        .build();
+                minioClient.statObject(statObjectArgs);
+
+                // Si no se lanza una excepción, el archivo existe
+                // Obtener el archivo desde MinIO y añadirlo a la lista
+                MultipartFile file = minioService.getFile(bucketName, multimediaEntity.getMultimedia());
+                multipartFiles.add(file);
+            } catch (MinioException | IOException | IllegalArgumentException e) {
+                // La excepción IllegalArgumentException se lanza si el objeto no existe
+                // Puedes manejarla aquí o simplemente omitir este archivo y continuar con los demás
+                continue; // Omitir este archivo y continuar con el siguiente
+            } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+                // Manejar otras excepciones aquí si es necesario
+                throw new RuntimeException(e);
+            }
         }
-        return multimediaDto;
+        return multipartFiles;
     }
+
+
 }
